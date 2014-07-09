d0cb3cf347d908829f515fee267304434ceaeae4
csbailey5t <csb5t@virginia.edu>
2014-07-09 14:42:45 -0400

    removed unnecessary lines(?) in script and put php variable in quotes - still doesn't work

--- OmekaAnalyticsPlugin.php
+++ OmekaAnalyticsPlugin.php
@@ -31,12 +31,8 @@ class OmekaAnalyticsPlugin extends Omeka_Plugin_AbstractPlugin
             $analyticsKey = get_option( 'analytics_key' ); //camelcase to use in JS
 
             $analytics_script = <<<ANALYTICS
-                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-                  ga('create', $analyticsKey, 'auto');
+                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                  ga('create', '$analyticsKey', 'auto')
                   ga('send', 'pageview');
 ANALYTICS;
 
